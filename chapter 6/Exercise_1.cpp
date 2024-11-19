6e9ab3a32dfd6e31196805527107060e14ce5b08
adelian14 <62575887+adelian14@users.noreply.github.com>
2024-11-20 00:54:51 +0200

    Update Exercise_1.cpp

--- chapter 6/Exercise_1.cpp
+++ chapter 6/Exercise_1.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
 
+using namespace std;
+
+class Int{
+    int val;
+public:
+    Int(){
+        val = 0;
+    }
+    Int(int x){
+        val = x;
+    }
+    void display(){
+        cout << val;
+    }
+    Int operator+(Int x){
+        Int ret = Int(val+x.val);
+        return ret;
+    }
+};
+
+int main(){
+    Int x,y(5),z(12);
+    x = y+z;
+    x.display();
+    return 0;
+}
