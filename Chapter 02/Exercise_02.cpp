d473e050b0454d885ffd2ddb1dbaa89d4de33143
adelian14 <62575887+adelian14@users.noreply.github.com>
2024-12-02 15:16:56 +0200

    Create Exercise_02.cpp

---
+++ Chapter 02/Exercise_02.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+#include <conio.h>
+#include <process.h>
+
+using namespace std;
+
+class sterling{
+
+};
+
+int main()
+{
+    cout    << 1990 << '\t' << setw(5) << 125 << '\n'
+            << 1991 << '\t' << setw(5) << 7290 << '\n'
+            << 1992 << '\t' << setw(5) << 11300 << '\n'
+            << 1993 << '\t' << setw(5) << 16200 << '\n';
+    return 0;
+}
