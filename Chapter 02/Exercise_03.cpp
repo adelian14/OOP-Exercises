4fc44b9c0f0c9210539cca7320c27a67fbc6b34f
adelian14 <62575887+adelian14@users.noreply.github.com>
2024-12-03 14:50:09 +0200

    Create Exercise_03.cpp

---
+++ Chapter 02/Exercise_03.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <conio.h>
+#include <process.h>
+
+using namespace std;
+
+int main()
+{
+    const int x = 10;
+    int y = 20;
+    cout << x << endl << y << endl << --y << endl;
+    return 0;
+}
