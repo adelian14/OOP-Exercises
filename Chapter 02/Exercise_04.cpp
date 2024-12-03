afde4a2c3ac9e8e20e2ec072e5a3f2e661ac0efa
adelian14 <62575887+adelian14@users.noreply.github.com>
2024-12-03 15:02:59 +0200

    Create Exercise_04.cpp

---
+++ Chapter 02/Exercise_04.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+#include <conio.h>
+#include <process.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Beneath the stars, the whispers glide,\nA fleeting truth where dreams reside,\nIn silence, worlds and hearts collide.";
+    return 0;
+}
